fcb160cadb511367f85f98c56825d857419782e6
opet <opet@Academico.edu>
2018-09-20 21:41:23 -0300

    up

---
+++ app/src/main/java/br/com/opet/tds/appregistro/MainActivity.java
@@ -0,0 +1,54 @@
+package br.com.opet.tds.appregistro;
+
+import android.app.Activity;
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private List<Artista> artistas;
+    private EditText editNome;
+    private Spinner spinnerGenero;
+    private ListView listArtista;
+    private ArrayAdapter<Artista> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        artistas = new ArrayList<>();
+        editNome = findViewById(R.id.editNome);
+        spinnerGenero = findViewById(R.id.spinnerGenero);
+        listArtista = findViewById(R.id.listArtista);
+        atualizarArtistas();
+    }
+
+    private void atualizarArtistas(){
+        if(adapter == null) {
+            adapter = new ArtistaAdapter(getApplicationContext(), R.layout.artista_item, artistas);
+            listArtista.setAdapter(adapter);
+        }else{
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    public void salvarArtista(View v){
+        String nome = editNome.getText().toString();
+        String genero = spinnerGenero.getSelectedItem().toString();
+        Artista a = new Artista();
+        a.setNome(nome);
+        a.setGenero(genero);
+
+        artistas.add(a);
+        atualizarArtistas();
+    }
+}
